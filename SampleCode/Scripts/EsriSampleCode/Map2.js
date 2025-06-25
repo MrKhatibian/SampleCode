4e00dd6f30dac1a021ea235bfee481ad478603a8
MrKhatibian <MrKhatibian@gmail.com>
2025-06-25 08:43:15 +0330

    Make Clustering

--- SampleCode/Scripts/EsriSampleCode/Map2.js
+++ SampleCode/Scripts/EsriSampleCode/Map2.js
@@ -1,8 +1,48 @@
 ﻿import Map from "../../esriapi/4.30/@arcgis/core/map.js";
 import MapView from "../../esriapi/4.30/@arcgis/core/views/mapview.js";
 import FeatureLayer from "../../esriapi/4.30/@arcgis/core/layers/featurelayer.js";
+
+
+/********************
+ * Add feature layer
+ ********************/
+const clusteredLayer = new FeatureLayer({
+    url: "http://localhost:6080/arcgis/rest/services/Maryanaj/Maryanaj/FeatureServer/1",
+    featureReduction: {
+        type: "cluster",
+        clusterRadius: "100px",
+        popupTemplate: {
+            title: "myCluster",
+            content: "testttt"
+        },
+        lablelingInfo: [{
+            deconflictionStrategy: "none",
+            lableExpressionInfo: {
+                expression: "$feature.cluster_count"
+            },
+            symbol: {
+                type: "text",
+                color: "white",
+                haloColor: "black",
+                haloSize: "1px",
+                font: {
+                    size: "12px",
+                    wight: "bold",
+                }
+            },
+            lablePlacement: "center-center"
+        }]
+    },
+    popupTemplate: {
+        title: "{name}",
+        content: "Naghshe Info"
+    },
+    outFields: ["*"]
+});
+
 const map = new Map({
-    basemap: "osm"
+    basemap: "osm",
+    layers: [clusteredLayer]
 });
 
 const view = new MapView({
@@ -11,15 +51,3 @@ const view = new MapView({
     zoom: 15, // Zoom level
     center: [48.464869, 34.834155], // Longitude, latitude 48.464869  34.834155
 });
-
-/********************
- * Add feature layer
- ********************/
-
-// Carbon storage of trees in Warren Wilson College.
-const featureLayer = new FeatureLayer({
-    url: "http://localhost:6080/arcgis/rest/services/Maryanaj/Maryanaj_14030619/FeatureServer/0",
-    outFields: ["*"]
-});
-
-map.add(featureLayer);
