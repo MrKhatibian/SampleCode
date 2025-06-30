7f02be82481fbf6bb3d5c44530afae65ed197097
MrKhatibian <MrKhatibian@gmail.com>
2025-06-30 14:49:34 +0330

    Add definitionExpression for Feature

--- SampleCode/Scripts/EsriSampleCode/Map3.js
+++ SampleCode/Scripts/EsriSampleCode/Map3.js
@@ -30,74 +30,81 @@ const clusteredLayer = new FeatureLayer({
             color: "black",
             outline: null
         }
-    },
-    featureReduction: {
-        type: "cluster",
-        clusterRadius: "100px",
-        clusterMaxSize: 40,
-        clusterMinSize: 10,
-        maxScale: 3000,   
-        renderer: {
-            type: "unique-value",
-            field: "cluster_count",
-            uniqueValueInfos: [
-                {
-                    value: 1,
-                    symbol: {
-                        type: "simple-marker",                        
-                        size: 5,
-                        outline: null // No outline for cluster_count = 1
-                    }
-                }
-            ],
-            defaultSymbol: {
-                type: "simple-marker",
-                color: "#BEE8FF",
-                size: 10,
-                outline: {
-                    type: "simple-line",
-                    color: [0, 77, 158, 0.5],
-                    width: 5
-                }
-            },
-            visualVariables: [
-                {
-                    type: "color",
-                    field: "cluster_count",
-                    stops: [
-                        { value: 1, color: "black" },
-                        { value: 2, color: "#BEE8FF" },
-                        { value: 100, color: "#002673" }
-                    ]
-                }
-            ]
-        },        
-        labelingInfo: [{
-            deconflictionStrategy: "none",
-            labelExpressionInfo: {
-                expression: "$feature.cluster_count"
-            },
-            symbol: {
-                type: "text",
-                color: "white",
-                haloColor: "black",
-                haloSize: "1px",
-                font: {
-                    family: "Arial",
-                    size: 10,
-                    weight: "bold"
-                }
-            },
-            labelPlacement: "center-center"
-        }]
-    },    
+    },   
     popupTemplate: {
         title: "درخواست",
         content: "شماره: {ShoD}"
     },
     outFields: ["*"]
 });
-
+clusteredLayer.featureReduction = {
+    type: "cluster",
+    clusterRadius: "100px",
+    clusterMaxSize: 40,
+    clusterMinSize: 10,
+    maxScale: 3000,
+    renderer: {
+        type: "unique-value",
+        field: "cluster_count",
+        uniqueValueInfos: [
+            {
+                value: 1,
+                symbol: {
+                    type: "simple-marker",
+                    size: 5,
+                    outline: null // No outline for cluster_count = 1
+                }
+            }
+        ],
+        defaultSymbol: {
+            type: "simple-marker",
+            color: "#BEE8FF",
+            size: 10,
+            outline: {
+                type: "simple-line",
+                color: [0, 77, 158, 0.5],
+                width: 3
+            }
+        },
+        visualVariables: [
+            {
+                type: "color",
+                field: "cluster_count",
+                stops: [
+                    { value: 1, color: "black" },
+                    { value: 2, color: "#BEE8FF" },
+                    { value: 100, color: "#002673" }
+                ]
+            }
+        ]
+    },
+    labelingInfo: [{
+        deconflictionStrategy: "none",
+        labelExpressionInfo: {
+            expression: "$feature.cluster_count"
+        },
+        symbol: {
+            type: "text",
+            color: "white",
+            haloColor: "black",
+            haloSize: "1px",
+            font: {
+                family: "Arial",
+                size: 10,
+                weight: "bold"
+            }
+        },
+        labelPlacement: "center-center"
+    }]
+};
+//document.getElementById("btnFilter").addEventListener("click", () => {   
+//    const value = document.getElementById("inFilter").value;  
+//    clusteredLayer.definitionExpression = value ? `ShoD > ${value}` : "";
+//});
+document.getElementById("inFilter").addEventListener("input", function () {
+    const value = this.value;
+    clusteredLayer.definitionExpression = value ? `ShoD > ${value}` : "";
+});
 const map = new Map({
     basemap: "osm",
     layers: [mapImageLayer, clusteredLayer]
