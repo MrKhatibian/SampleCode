f4e1ff19ea6bc89b79fee4539a88962195e48fbc
MrKhatibian <MrKhatibian@gmail.com>
2025-08-07 11:36:33 +0330

    fixed load layer bugs

--- SampleCode/Scripts/EsriSampleCode/Map1.js
+++ SampleCode/Scripts/EsriSampleCode/Map1.js
@@ -5,7 +5,9 @@ import GraphicsLayer from "../../EsriAPI/4.30/@arcgis/core/layers/GraphicsLayer.
 import * as geometryEngine from "../../EsriAPI/4.30/@arcgis/core/geometry/geometryEngine.js";
 import Point from "../../EsriAPI/4.30/@arcgis/core/geometry/Point.js";
 import Graphic from "../../EsriAPI/4.30/@arcgis/core/Graphic.js";
-import Query from "../../EsriAPI/4.30/@arcgis/core/rest/support/Query.js"
+import Query from "../../EsriAPI/4.30/@arcgis/core/rest/support/Query.js";
+import MapImageLayer from "../../EsriAPI/4.30/@arcgis/core/layers/MapImageLayer.js";
+
 
 const map = new Map({ basemap: "osm" });
 
@@ -18,17 +20,11 @@ const view = new MapView({
 
 const url = "http://localhost:6080/arcgis/rest/services/Maryanaj/MaryanajNN/MapServer";
 
-const darkhastFLayer = new FeatureLayer({
-    url: `${url}+/0`,
+const darkhastFLayer = new FeatureLayer({url: `${url}/0`});
 
-});
-
-const arseFLayer = new FeatureLayer({
-    url: `${url}+/1`,
-});
+const arseFLayer = new FeatureLayer({ url: `${url}/1`});
 
 const graphicsLayer = new GraphicsLayer();
-
 
 map.addMany([arseFLayer, darkhastFLayer, graphicsLayer]);
 
@@ -83,6 +79,7 @@ async function generateValidPointInPolygon(polygon) {
     console.warn("Failed to generate a unique point.");
     return null;
 }
+
 // Main logic
 async function createDarkhastPoint(nCode) {
     debugger;
@@ -110,7 +107,7 @@ async function createDarkhastPoint(nCode) {
 
     console.log("Generated point geometry:", randomPoint.toJSON());
 }
-document.getElementById("btnSabtDarkhast").addEventListener("click", () => {
-    alert("Hi Mohammad");
+
+document.getElementById("btnSabtDarkhast").addEventListener("click", () => {    
     createDarkhastPoint("501-3-13-1-0-0-0");
 })
