225fdde916516c7c2b071893ee3cd912c02a97f2
MrKhatibian <MrKhatibian@gmail.com>
2025-08-11 15:55:53 +0330

    Add Connection to db

--- SampleCode/Controllers/HomeController.cs
+++ SampleCode/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
 
 namespace SampleCode.Controllers
 {
@@ -8,7 +11,6 @@ namespace SampleCode.Controllers
         {
             return View();
         }
-
         public ActionResult Map1()
         {
             return View();
@@ -28,6 +30,28 @@ namespace SampleCode.Controllers
         public ActionResult Map5()
         {
             return View();
+        }
+        [HttpGet]
+        public JsonResult testConnection()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["AmardShahrsaziMaryanaj"].ConnectionString;
+            string message;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    message = "✅ Connection Successful: " + conn.ServerVersion;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = "❌ Connection failed: " + ex.Message;
+            }
+
+            return Json(new { result = message }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
